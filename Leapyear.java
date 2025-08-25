91d69206036c786b1230c7c3151938d123c6821a
yerukulapavan <152668837+yerukulapavan@users.noreply.github.com>
2025-08-24 20:59:16 -0500

    Added leap year code

---
+++ Leapyear.java
@@ -0,0 +1,30 @@
+package javaproject;
+
+public class Leapyear {
+
+    public static void main(String[] args) {
+        int year = 2023; // Change this to test different years
+
+        if (isLeapYear(year)) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+    }
+
+    public static boolean isLeapYear(int year) {
+        if (year < 1) {
+            return false;
+        }
+
+        if (year % 4 != 0) {
+            return false;
+        } else if (year % 100 != 0) {
+            return true;
+        } else if (year % 400 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
